ad55b702751b7946f0a105ef4c6002ff926f67e5
1seyoung <20171631@edu.hanbat.ac.kr>
2024-11-18 01:09:57 +0900

    Refactor : 시스템 구조 변환 - app : 백엔드 - streamplit app : 프론트 - db : mysql - 주요 로직을 위한 시스템 구조 변환 ( 컨텐츠 관리, 추천, 검색이 가능하게 하고 외부 라이브러리보다는 직접 구현해보고 추후에 수정하는 방향으로)

---
+++ app/core/config.py
@@ -0,0 +1,10 @@
+from pandantic import BaseSettings
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+class Settings(BaseSettings):
+    DATABASE_URL: str = os.getenv("DATABASE_URL")
+
+settings = Settings()
---
+++ app/core/database.py
@@ -0,0 +1,5 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+#orm 사용 x 쿼리 연습
+
